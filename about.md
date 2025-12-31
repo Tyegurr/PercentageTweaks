447863faf33efa313d9ef62a1bfd5b0d0b262bdf
Tyegurr <okaypolo44@gmail.com>
2025-12-31 17:58:46 -0500

    Initial Commit

---
+++ about.md
@@ -0,0 +1,11 @@
+# Percentage tweaks
+Lets you change how the percentage label is displayed.\
+Settings available:
+- Percent Precision
+- Scientific Notation
+- Percent scale
+
+# Honourable mentions:
+- Klaif Niankovsky
+- Cosmicrown
+- pipsterzz
---
+++ src/PlayLayerHook.hpp
@@ -0,0 +1,38 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(PlayLayerHook, PlayLayer) {
+    struct Fields {
+        long double m_longPercentage;
+        int m_percentagePrecision;
+        bool m_scientificNotation;
+    };
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+
+        m_fields->m_percentagePrecision = Mod::get()->getSettingValue<int>("percentage-precision");
+        m_fields->m_scientificNotation = Mod::get()->getSettingValue<bool>("scientific-notation");
+
+        // setting up percentage label
+        m_percentageLabel->setAnchorPoint({0.5, 1.0});
+        m_percentageLabel->setPositionY(m_percentageLabel->getPositionY() + 8.0f);
+        m_percentageLabel->setScale(Mod::get()->getSettingValue<float>("percentage-label-scale"));
+
+        return true;
+    }
+    void updateProgressbar() {
+        PlayLayer::updateProgressbar();
+
+        // percentage calculators happen here
+        cocos2d::CCPoint realPlayerPosition = m_player1->getRealPosition();
+        m_fields->m_longPercentage = (static_cast<long double>(realPlayerPosition.x) / static_cast<long double>(m_endXPosition)) * 100.0L;
+        // now we can set the percentage label text
+        if (m_fields->m_scientificNotation == true) {
+            m_percentageLabel->setString(fmt::format("{:.{}e}%", m_fields->m_longPercentage, m_fields->m_percentagePrecision).c_str());
+        } else {
+            m_percentageLabel->setString(fmt::format("{:.{}f}%", m_fields->m_longPercentage, m_fields->m_percentagePrecision).c_str());
+        }
+    }
+};
